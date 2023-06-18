3cd706c1a7a1c54ed2ff39d2a03c05f95316e509
name <srpiroliro21@gmail.com>
2023-06-19 00:50:20 +0200

    added api application

---
+++ src/main/java/com/tecnocampus/banktcm/BankApiApplication.java
@@ -0,0 +1,11 @@
+package com.tecnocampus.banktcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BankApiApplication.class, args);
+    }
+}
